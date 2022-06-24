fda54892186a744d0d3eb666a48aaeebc714556e
Duane McGuire <biz@duanemcguire.com>
2022-06-24 11:35:54 -0600

    nuxt-config

--- components/topnav.vue
+++ components/topnav.vue
@@ -3,7 +3,7 @@
   <a href="/" class="home">Home</a>
   <a href="/schedule" class="schedule">Schedule</a>
   <a href="/passion" class="passion">Passion</a>
-  <a href="about" class="about">About</a>
+  <a href="/about" class="about">About</a>
   <a href="/contact" class="contact">Contact</a>
 </div>
 </template>
