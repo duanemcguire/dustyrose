06dcf7952d4d2e1137da83ce795ab28d1aed3f59
Duane McGuire <biz@duanemcguire.com>
2022-07-23 15:20:33 -0600

    add blog to menu

--- components/topnav.vue
+++ components/topnav.vue
@@ -4,6 +4,7 @@
   <a href="/schedule" class="schedule">Schedule</a>
   <a href="/passion" class="passion">Passion</a>
   <a href="/about" class="about">About</a>
+  <a href="/blog" class="about">Blog</a>
   <a href="/contact" class="contact">Contact</a>
 </div>
 </template>
